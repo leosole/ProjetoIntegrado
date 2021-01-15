7c2a5219984e8e6ef1c218622efec702c75ca50e
leosole <leosole@gmail.com>
2021-01-15 18:10:43 -0300

    ajuste action bar

--- app/src/main/java/com/ufrj/projetointegrado/MainActivity.kt
+++ app/src/main/java/com/ufrj/projetointegrado/MainActivity.kt
@@ -15,7 +15,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
+        setSupportActionBar(binding.myToolbar)
     }
     fun showUpButton() {  // Mostra seta para voltar
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
